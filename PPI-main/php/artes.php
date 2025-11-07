798a2c546ae1fc814a8c0607a394355265ac18b5
louizthelyricist <luiz.60063@aluno.iffar.edu.br>
2025-11-07 15:03:20 -0300

    Update artes.php

--- PPI-main/php/artes.php
+++ PPI-main/php/artes.php
@@ -16,7 +16,7 @@ $a2 = $_GET ["a2"];
 $a3 = $_GET ["a3"];
 $a4 = $_GET ["a4"];
 $a5 = $_GET ["a5"];
-    echo "Matéria: Artes<br><br>";
+    echo "<legend>Matéria: Artes</legend>";
     $a1= $_GET ["a1"];
     if ($a1==1)
     {
